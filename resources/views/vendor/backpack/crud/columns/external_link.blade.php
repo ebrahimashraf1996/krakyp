6ea642c2aecf5c3e2cb55526d8b7b1f3d55454fb
Ebrahim Ashraf <ebrahimashraf1996@gmail.com>
2023-07-08 17:11:01 +0300

    fix issue in Messages Crud List

--- resources/views/vendor/backpack/crud/columns/external_link.blade.php
+++ resources/views/vendor/backpack/crud/columns/external_link.blade.php
@@ -1,13 +1,26 @@
 @if($entry->reason == 'إبلاغ عن إعلان')
-@php
-    $client_ad = \App\Models\Clientad::where('serial_num', $entry->serial_num)->first();
-@endphp
-    <span><a href="{{route('client_ad.show', $client_ad->slug)}}" target="_blank">الإعلان</a></span>
+    @php
+        $client_ad = \App\Models\Clientad::where('serial_num', $entry->serial_num)->first();
+    @endphp
+    @if($client_ad)
+        <span><a href="{{route('client_ad.show', $client_ad->slug)}}" target="_blank">الإعلان</a></span>
+    @else
+        <span>اعلان محذوف</span>
+    @endif
+
+
+
 @elseif($entry->reason == 'إبلاغ عن بائع')
     @php
         $user = \App\Models\User::where('serial_num', $entry->serial_num)->first();
     @endphp
-    <span><a href="{{backpack_url('user/' . $user->id . '/show')}}" target="_blank">البائع</a></span>
+    @if($user)
+        <span><a href="{{backpack_url('user/' . $user->id . '/show')}}" target="_blank">البائع</a></span>
+
+    @else
+        <span>تم حذف المعلن</span>
+    @endif
+
 @else
     <span>---</span>
 
